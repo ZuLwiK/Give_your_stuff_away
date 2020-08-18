78757eca1dcda61ffd443a65e483d495cac84a98
ZuLwiK <smolorz.michal@gmail.com>
2020-08-18 21:28:01 +0200

    initial commit

---
+++ src/main/java/pl/zulwik/give_your_stuff_away/GiveYourStuffAwayApplication.java
@@ -0,0 +1,13 @@
+package pl.zulwik.give_your_stuff_away;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveYourStuffAwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiveYourStuffAwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/zulwik/give_your_stuff_away/GiveYourStuffAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zulwik.give_your_stuff_away;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveYourStuffAwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
